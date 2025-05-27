e484f308d9af23ffe295e409ff9c29955908120a
Evening Doggie <04.sunrise_picnics@icloud.com>
2025-05-28 02:35:39 +0300

    hash_adapter: Big int division with remainder

--- bloom_filter/hasher_adapter/include/hasher_adapter.hpp
+++ bloom_filter/hasher_adapter/include/hasher_adapter.hpp
@@ -3,37 +3,40 @@
 #include <array>
 #include <stdexcept>
 #include <openssl/md5.h>
+#include <openssl/bn.h>
 
 template<typename hasher_type>
 class hasher_adapter final
 {
-    int get_hash_code(
+    unsigned long long get_hash_code(
         const std::array<
             unsigned char,
             hasher_type::hash_size>& hash,
-        int const n)
+        unsigned long long const n)
     {
         /*if (hasher_type::hash_size==0 || hasher_type::hash_size % 2 != 0)
         {
             throw std::invalid_argument("Hash size must be a multiple of 2 and more than zero");
         }*/
 
-        //Error at compile time. 8 - bits in byte
-        static_assert(hasher_type::hash_size==0 || hasher_type::hash_size % 8 != 0, "Hash size must be a multiple of 8 and more than zero");
 
-        uint64_t hash_combined = 0;
+        BIGNUM* bn = BN_new();
+        BN_bin2bn(hash.data(), hash.size(), bn);
 
-        for (int i = 0; i < hasher_type::hash_size; i+=8)
-        {
-            uint64_t hash_8_bits = 0;
-            for (int j = i; j < i + 8; ++j)
-            {
-                hash_8_bits = (hash_8_bits << 8) | hash[j];
-            }
-            hash_combined ^= hash_8_bits;
-        }
+        // Модуль
+        BIGNUM* mod = BN_new();
+        BN_set_word(mod, 1024); // остаток по модулю 1024
 
-        uint64_t const mask = (1ULL << n) - 1;
-        return static_cast<int>(hash_combined & mask);
+        BIGNUM* result = BN_new();
+        BN_CTX* ctx = BN_CTX_new();
+
+        BN_mod(result, bn, mod, ctx);
+
+        BN_free(bn);
+        BN_free(mod);
+        BN_free(result);
+        BN_CTX_free(ctx);
+
+        return BN_get_word(result);
     }
 };
